4ad87ddc6522e64b3ff0a987d20f5b3861aec5ea
oqpvc <5766455+o-tho@users.noreply.github.com>
2025-05-08 00:25:09 +0300

    surpress debug messages, only log info and higher

--- src/main.rs
+++ src/main.rs
@@ -224,7 +224,7 @@ fn main() {
     use autograder::webapp::WebApp;
     use eframe::wasm_bindgen::JsCast;
     // Redirect `log` message to `console.log` and friends:
-    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+    eframe::WebLogger::init(log::LevelFilter::Info).ok();
 
     let web_options = eframe::WebOptions::default();
 
